a9c0d20414617daf94fbbd2c43f34cd6e007e22b
Alfredo Alas <alfredo.alas14@gmail.com>
2024-06-05 12:08:55 -0600

    add project files

---
+++ src/main/java/com/curso/microservicios/spring/app/usuarios/CursoMicroserviciosSpringMicroservicioUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.spring.app.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMicroserviciosSpringMicroservicioUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMicroserviciosSpringMicroservicioUsuariosApplication.class, args);
+	}
+
+}
