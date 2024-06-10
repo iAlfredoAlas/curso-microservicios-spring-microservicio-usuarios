529f04aa020978370d005ea9a6891abc30401541
Alfredo Alas <alfredo.alas14@gmail.com>
2024-06-09 18:49:47 -0600

    change imports

--- src/main/java/com/curso/microservicios/spring/app/usuarios/models/entity/Alumno.java
+++ src/main/java/com/curso/microservicios/spring/app/usuarios/models/entity/Alumno.java
@@ -2,31 +2,31 @@ package com.curso.microservicios.spring.app.usuarios.models.entity;
 
 import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "alumnos")
 public class Alumno {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long id;	
 	
 	@Column(name = "nombre")
 	private String nombre;
-	
+
 	@Column(name = "apellido")
 	private String apellido;
-	
-	@Column(name = "email")
+
+	@Column(name =  "email")
 	private String email;
-	
+
 	@Column(name = "create_at")
 	private Date createAt;
 	
