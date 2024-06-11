b57f24c2864428ad16414fe90dcb4f9df69ad9ba
Alfredo Alas <alfredo.alas14@gmail.com>
2024-06-11 11:47:34 -0600

    add configuration to dotenv java

--- src/main/java/com/curso/microservicios/spring/app/usuarios/CursoMicroserviciosSpringMicroservicioUsuariosApplication.java
+++ src/main/java/com/curso/microservicios/spring/app/usuarios/CursoMicroserviciosSpringMicroservicioUsuariosApplication.java
@@ -2,11 +2,16 @@ package com.curso.microservicios.spring.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class CursoMicroserviciosSpringMicroservicioUsuariosApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
 		SpringApplication.run(CursoMicroserviciosSpringMicroservicioUsuariosApplication.class, args);
 	}
 
