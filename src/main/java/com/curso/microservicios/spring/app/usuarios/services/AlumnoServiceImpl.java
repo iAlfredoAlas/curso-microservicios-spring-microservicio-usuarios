49b6b4b052b5df13ae3cb92d3ef2f639d251f024
Alfredo Alas <alfredo.alas14@gmail.com>
2024-06-23 17:04:25 -0600

    refactor CommonServiceImpl implementation

--- src/main/java/com/curso/microservicios/spring/app/usuarios/services/AlumnoServiceImpl.java
+++ src/main/java/com/curso/microservicios/spring/app/usuarios/services/AlumnoServiceImpl.java
@@ -1,42 +1,12 @@
 package com.curso.microservicios.spring.app.usuarios.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.curso.microservicios.spring.app.usuarios.models.entity.Alumno;
 import com.curso.microservicios.spring.app.usuarios.models.repository.AlumnoRepository;
+import com.curso.microservicios.spring.commons.services.CommonServiceImpl;
 
 @Service
-public class AlumnoServiceImpl implements AlumnoService {
-	
-	@Autowired
-	private AlumnoRepository alumnoRepository;
-
-	@Override
-	@Transactional(readOnly = true)
-	public Iterable<Alumno> findall() {
-		return alumnoRepository.findAll();
-	}
-
-	@Override
-	@Transactional(readOnly = true)
-	public Optional<Alumno> findbyId(Long id) {
-		return alumnoRepository.findById(id);
-	}
-
-	@Override
-	@Transactional
-	public Alumno save(Alumno alumno) {
-		return alumnoRepository.save(alumno);
-	}
-
-	@Override
-	@Transactional
-	public void deleteById(Long id) {
-		alumnoRepository.deleteById(id);
-	}
+public class AlumnoServiceImpl extends CommonServiceImpl<Alumno, AlumnoRepository> implements AlumnoService {
 
 }
