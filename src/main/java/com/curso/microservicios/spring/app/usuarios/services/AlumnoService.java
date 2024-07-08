68184dfd3e0b770dfcd162545ecd1896714b85c6
Alfredo Alas <alfredo.alas14@gmail.com>
2024-07-08 16:36:48 -0600

    implement method findByNombreOrApellido

--- src/main/java/com/curso/microservicios/spring/app/usuarios/services/AlumnoService.java
+++ src/main/java/com/curso/microservicios/spring/app/usuarios/services/AlumnoService.java
@@ -1,8 +1,12 @@
 package com.curso.microservicios.spring.app.usuarios.services;
 
+import java.util.List;
+
 import com.curso.microservicios.spring.commons.services.CommonService;
 import com.curso.microservicios.spring.generic.alumnos.models.entity.Alumno;
 
 public interface AlumnoService extends CommonService<Alumno> {
 
+	public List<Alumno> findByNombreOrApellido(String term);
+	
 }
--- src/main/java/com/curso/microservicios/spring/app/usuarios/services/AlumnoServiceImpl.java
+++ src/main/java/com/curso/microservicios/spring/app/usuarios/services/AlumnoServiceImpl.java
@@ -1,6 +1,9 @@
 package com.curso.microservicios.spring.app.usuarios.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.curso.microservicios.spring.app.usuarios.models.repository.AlumnoRepository;
 import com.curso.microservicios.spring.commons.services.CommonServiceImpl;
@@ -8,5 +11,11 @@ import com.curso.microservicios.spring.generic.alumnos.models.entity.Alumno;
 
 @Service
 public class AlumnoServiceImpl extends CommonServiceImpl<Alumno, AlumnoRepository> implements AlumnoService {
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Alumno> findByNombreOrApellido(String term) {
+		return repository.findByNombreOrApellido(term);
+	}
 
 }
