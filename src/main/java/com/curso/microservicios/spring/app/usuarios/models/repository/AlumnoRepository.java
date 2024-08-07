095c893fab1ffba378ee2d838954e12fca43ce86
Alfredo Alas <alfredo.alas14@gmail.com>
2024-08-07 10:08:05 -0600

    change extends to PagingAndSortingRepository

--- src/main/java/com/curso/microservicios/spring/app/usuarios/models/repository/AlumnoRepository.java
+++ src/main/java/com/curso/microservicios/spring/app/usuarios/models/repository/AlumnoRepository.java
@@ -3,11 +3,11 @@ package com.curso.microservicios.spring.app.usuarios.models.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.curso.microservicios.spring.generic.alumnos.models.entity.Alumno;
 
-public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
+public interface AlumnoRepository extends PagingAndSortingRepository<Alumno, Long> {
 
 	@Query("select alu from Alumno alu where alu.nombre like %?1% or alu.apellido like %?1%")
 	public List<Alumno> findByNombreOrApellido(String term);
