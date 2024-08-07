eeb0f4884a006d802dc676b92874c2e37d5c5029
Alfredo Alas <alfredo.alas14@gmail.com>
2024-08-07 10:59:51 -0600

    add crearConFoto and editarConFoto endpoints

--- src/main/java/com/curso/microservicios/spring/app/usuarios/controllers/AlummnoController.java
+++ src/main/java/com/curso/microservicios/spring/app/usuarios/controllers/AlummnoController.java
@@ -1,5 +1,6 @@
 package com.curso.microservicios.spring.app.usuarios.controllers;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -9,9 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.curso.microservicios.spring.app.usuarios.services.AlumnoService;
 import com.curso.microservicios.spring.commons.controllers.CommonController;
@@ -39,6 +43,35 @@ public class AlummnoController extends CommonController<Alumno, AlumnoService> {
 	@GetMapping("/filtrar/{term}")
 	public ResponseEntity<?> filtrar(@PathVariable String term) {
 		return ResponseEntity.ok(service.findByNombreOrApellido(term));
+	}
+
+	@PostMapping("/crear-con-foto")
+	public ResponseEntity<?> crearConFoto(@Valid Alumno alumno, BindingResult result,
+			@RequestParam MultipartFile archivo) throws IOException {
+		if (!archivo.isEmpty()) {
+			alumno.setFoto(archivo.getBytes());
+		}
+		return super.crear(alumno, result);
+	}
+
+	@PutMapping("/editar-con-foto/{id}")
+	public ResponseEntity<?> editarConFoto(@Valid Alumno alumno, BindingResult result, @PathVariable Long id,
+			@RequestParam MultipartFile archivo) throws IOException {
+		if (result.hasErrors()) {
+			return this.validar(result);
+		}
+		Optional<Alumno> optAlumno = service.findbyId(id);
+		if (optAlumno.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		Alumno alumnoDb = optAlumno.get();
+		alumnoDb.setNombre(alumno.getNombre());
+		alumnoDb.setApellido(alumno.getApellido());
+		alumnoDb.setEmail(alumno.getEmail());
+		if (!archivo.isEmpty()) {
+			alumnoDb.setFoto(archivo.getBytes());
+		}
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDb));
 	}
 
 }
